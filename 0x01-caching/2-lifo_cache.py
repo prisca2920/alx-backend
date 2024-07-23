ce3db8ae2c336c2c0f18d9708d48fdcb71efbb57
prisca2920 <prisca2920@gmail.com>
2024-07-23 17:53:58 +0000

    lifo

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""lifo caching system"""
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """fifo caching system"""
+    def __init__(self):
+        """initializing the class"""
+        super().__init__()
+        self.key_index = []
+
+    def put(self, key, item):
+        """assigning key to item"""
+        if item is None or key is None:
+            pass
+        else:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS \
+                    and key not in self.cache_data:
+                item_deleted = self.key_index.pop()
+                print("DISCARD:", item_deleted)
+                del self.cache_data[item_deleted]
+
+            self.cache_data[key] = item
+            self.key_index.append(key)
+
+    def get(self, key):
+        """gets the key"""
+        if key is None or key not in self.cache_data.keys():
+            return None
+        return self.cache_data.get(key)
