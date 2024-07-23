fd698e22bd0fda2bbb384979e5a295699a9e1aee
prisca2920 <prisca2920@gmail.com>
2024-07-23 16:43:02 +0000

    basic dict

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""creating a basic dictionary"""
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """creating a basic caching class"""
+    def __init__(self):
+        """initializing the class"""
+        super().__init__()
+
+    def put(self, key, item):
+        """assigning the key and item"""
+        if key and item:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """returns the key"""
+        if key is None or key not in self.cache_data.keys():
+            return None
+        return self.cache_data.get[key]
